fc59db4d99a1dea8cf04e232ab7131b086129de3
cyicz123 <cyicz123@outlook.com>
2025-04-22 20:17:59 +0800

    fix: 修复了找不到vgg模型的问题

--- dl_framework/models/__init__.py
+++ dl_framework/models/__init__.py
@@ -1,9 +1,11 @@
 from .base_model import BaseModel
 from .registry import ModelRegistry
 from .cnn import CNN
+from .vgg16 import VGG16
 
 __all__ = [
     'BaseModel',
     'ModelRegistry',
     'CNN',
+    'VGG16',
 ]

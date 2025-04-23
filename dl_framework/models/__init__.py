a39b72d7c23bac9c72bf3f5210e88c4ec94be03f
zhaozhenbang <32409080@mail.imu.edu.cn>
2025-04-23 20:29:42 +0800

    feat: 添加了一个新模型densenet和模型评价指标 #7

--- dl_framework/models/__init__.py
+++ dl_framework/models/__init__.py
@@ -2,10 +2,12 @@ from .base_model import BaseModel
 from .registry import ModelRegistry
 from .cnn import CNN
 from .vgg16 import VGG16
+from .densenet import DenseNet
 
 __all__ = [
     'BaseModel',
     'ModelRegistry',
     'CNN',
     'VGG16',
+    'DenseNet',
 ]
